1c77c3609ef3e97772d72e3c4541493a6453c966
lisongyu <lisongyu@jisuanke.com>
2016-06-01 18:14:55 +0800

    fixed ReadMe file

--- index.js
+++ index.js
@@ -1,21 +1,29 @@
 
 var argv = process.argv;
 if(argv[2]=='--help'){
-	console.log('This is a spider program that helps you grab text information online. Your final results are in ./artical.txt.\nYou can enter your target website domain as the third argument, first webpage to parse as the forth argument, number of entries you need as your fifth argument, and feel free to add any jquery selector in the end, but please be aware that if your selector is wrong, you may end up with a empty article.txt.');
+	console.log('This is a spider program that helps you grab text information online. If you choose to grab text, your final results are in ./artical.txt.');
+	console.log('If you choose to grab images, your results will be shown in directory /images');
+	console.log('Text Usage: node ./index.js [domain] [url] [num of entries needed] [jquery selector] ...\nEnter --help for more info');
+	console.log('Image Usage: node ./index.js --pic [domain] [url] [num of albums] [jquery selectors] ...\nEnter --help for more info');
 	process.exit();
 }
 
-if(argv[2]=='--pic'){	//image crawler mode
-	
+else if(argv[2]=='--pic'){
+	//image crawler mode
+	if(argv.length != 7){
+		console.log('Usage: node ./index.js --pic [domain] [url] [num of albums] [jquery selectors] ...\nEnter --help for more info');
+		process.exit();
+	}
+	//var Image_Parser = require('./lib/image.js').init(/*arguments*/);
 }
 else{
 	if(count <= 5){
-		console.log('Usage: node ./index.js [domain] [url] [num of entries needed] [jquery selector...]\nEnter --help for more info');
+		console.log('Usage: node ./index.js [domain] [url] [num of entries needed] [jquery selector] ...\nEnter --help for more info');
 		process.exit(1);
 	}
 	console.log('MeinSpider Copyright (C) 2016 Songyu Li');
 
-	var count = argv.length;			
+	var count = argv.length;
 	var jquery = '';
 	var i = 5;
 	while(i<count){
