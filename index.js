dbee6b2b2a55089e2629abcfdb336dfba96cdb1d
Songyu Li <lisongyu2009@gmail.com>
2017-09-11 16:26:44 -0500

    done

--- index.js
+++ index.js
@@ -1,35 +1,36 @@
 
 var argv = process.argv;
+var count = argv.length;
+
 if(argv[2]=='--help'){
 	console.log('This is a crawler program that helps you grab text information online. If you choose to grab text, your final results are in ./artical.txt.');
 	console.log('If you choose to grab images, your results will be shown in directory /images');
-	console.log('Text Usage: node ./index.js [domain] [url] [num of entries needed] [jquery selector] ...\nEnter --help for more info');
-	console.log('Image Usage: node ./index.js --pic [domain] [url] [num of albums] [jquery selectors] ...\nEnter --help for more info');
-	process.exit();
+	console.log('Usage: node ./index.js [domain] [url] [num of items needed] [jquery selector]');
+	console.log('f save items as files');
+    process.exit()
 }
 
-else if(argv[2]=='--pic'){
-	//image crawler mode
-	if(argv.length != 7){
-		console.log('Usage: node ./index.js --pic [domain] [url] [num of albums] [jquery selectors] ...\nEnter --help for more info');
-		process.exit();
-	}
-	//var Image_Parser = require('./lib/image.js').init(/*arguments*/);
+else if(count <= 5){
+    console.log('Command Error.');
+    console.log('Type --help for more info.')
+    process.exit()
 }
 else{
-	if(count <= 5){
-		console.log('Usage: node ./index.js [domain] [url] [num of entries needed] [jquery selector] ...\nEnter --help for more info');
-		process.exit(1);
-	}
-	console.log('MeinSpider Copyright (C) 2016 Songyu Li');
+	var fileMode = false
+	var securityMode = false
+	argv.forEach(function (t) {
+		if(t == 'f'){
+			fileMode = true
+			argv.splice(argv.indexOf(t))
+		}
+	})
 
-	var count = argv.length;
+	console.log('MeinSpider Copyright (C) 2016 Songyu Li');
 	var jquery = '';
 	var i = 5;
 	while(i<count){
 		jquery += (' '+argv[i]);
 		i++;
 	}
-
 	var Parser = require('./lib/parse.js').init(argv[2], argv[3], argv[4], jquery);
 }
